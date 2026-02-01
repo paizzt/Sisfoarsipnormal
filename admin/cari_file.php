a204c31a7af8c2f160413ee6312feebf1e988860
paizzt <martevenfaisal@gmail.com>
2026-02-01 19:03:37 +0800

    title

--- admin/cari_file.php
+++ admin/cari_file.php
@@ -35,7 +35,8 @@ if(isset($_GET['hapus_file'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cari Arsip - SISFO</title>
+    <title>Sisfo Arsip </title>
+    <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
